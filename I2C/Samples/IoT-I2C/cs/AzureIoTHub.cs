b640cecb5d0b44c155442f930f739221a03c98b6
darkMinds <eduarddavj@outlook.co.il>
2017-09-03 17:20:21 +0300

    commit i2c

---
+++ I2C/Samples/IoT-I2C/cs/AzureIoTHub.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+
+namespace SDKTemplate
+{
+    class AzureIoTHub
+    {
+        private static string DeviceId = "myFirstDevice"; //consider updating it to be the motitoredPlantID - it's unique
+        private static void CreateClient()
+        {
+            if (deviceClient == null)
+            {
+                // create Azure IoT Hub client from embedded connection string
+                deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+            }
+        }
+
+        static DeviceClient deviceClient = null;
+
+        //
+        // Note: this connection string is specific to the device "myFirstDevice". To configure other devices,
+        // see information on iothub-explorer at http://aka.ms/iothubgetstartedVSCS
+        //
+        const string deviceConnectionString = "HostName=GreenHouse-IOT-HUB.azure-devices.net;DeviceId=myFirstDevice;SharedAccessKey=BNKN9mefkcawRIC6KhJy+MctdJ00OVgh3+DOtaOxOJM=";
+
+
+        //
+        // To monitor messages sent to device "kraaa" use iothub-explorer as follows:
+        //    iothub-explorer monitor-events --login HostName=GreenHouse-IOT-HUB.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=Zs2qc/8niQWL1yuMmerBrGOZwboX5m1yvQobs6+UbRM= "myFirstDevice"
+        //
+
+        // Refer to http://aka.ms/azure-iot-hub-vs-cs-2017-wiki for more information on Connected Service for Azure IoT Hub
+
+        public static async Task SendDeviceToCloudMessageAsync()
+        {
+            CreateClient();
+
+            var currentTemperature = 20 /*getCurrentTemperature()*/;
+            var currentHumidity = 20/*getCurrentHumidity()*/;
+
+            var telemetryDataPoint = new
+            {
+                deviceId = DeviceId,
+                plantID = 7,
+                temperature = currentTemperature,
+                humidity = currentHumidity,
+                userId = 1
+            };
+            var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+            var message = new Message(Encoding.ASCII.GetBytes(messageString));
+            message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+            Debug.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+            /*
+    #if WINDOWS_UWP
+            var str = "{\"deviceId\":\"myFirstDevice\",\"messageId\":1,\"text\":\"Hello, Cloud from a UWP C# app!\"}";
+    #else
+            var str = "{\"deviceId\":\"myFirstDevice\",\"messageId\":1,\"text\":\"Hello, Cloud from a C# app!\"}";
+    #endif
+            var message = new Message(Encoding.ASCII.GetBytes(str));*/
+
+            await deviceClient.SendEventAsync(message);
+        }
+
+        public static async Task<string> ReceiveCloudToDeviceMessageAsync()
+        {
+            CreateClient();
+
+            while (true)
+            {
+                var receivedMessage = await deviceClient.ReceiveAsync();
+
+                if (receivedMessage != null)
+                {
+                    var messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                    await deviceClient.CompleteAsync(receivedMessage);
+                    return messageData;
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+
+        private static async Task<MethodResponse> OnSampleMethod1Called(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine("SampleMethod1 has been called");
+            return new MethodResponse(200);
+        }
+
+        private static async Task<MethodResponse> OnSampleMethod2Called(MethodRequest methodRequest, object userContext)
+        {
+            Console.WriteLine("SampleMethod2 has been called");
+            return new MethodResponse(200);
+        }
+
+        public static async Task RegisterDirectMethodsAsync()
+        {
+            CreateClient();
+
+            Console.WriteLine("Registering direct method callbacks");
+            await deviceClient.SetMethodHandlerAsync("SampleMethod1", OnSampleMethod1Called, null);
+            await deviceClient.SetMethodHandlerAsync("SampleMethod2", OnSampleMethod2Called, null);
+        }
+
+        public static async Task GetDeviceTwinAsync()
+        {
+            CreateClient();
+
+            Console.WriteLine("Getting device twin");
+            Twin twin = await deviceClient.GetTwinAsync();
+            Console.WriteLine(twin.ToJson());
+        }
+
+        private static async Task OnDesiredPropertiesUpdated(TwinCollection desiredProperties, object userContext)
+        {
+            Console.WriteLine("Desired properties were updated");
+            Console.WriteLine(desiredProperties.ToJson());
+        }
+
+        public static async Task RegisterTwinUpdateAsync()
+        {
+            CreateClient();
+
+            Console.WriteLine("Registering Device Twin update callback");
+            await deviceClient.SetDesiredPropertyUpdateCallback(OnDesiredPropertiesUpdated, null);
+        }
+
+        public static async Task UpdateDeviceTwin()
+        {
+            CreateClient();
+
+            TwinCollection tc = new TwinCollection();
+            tc["SampleProperty1"] = "test value";
+
+            Console.WriteLine("Updating Device Twin reported properties");
+            await deviceClient.UpdateReportedPropertiesAsync(tc);
+        }
+    }
+}
---
+++ I2C/Samples/IoT-I2C/cs/Scenario1_ReadData.xaml.cs
@@ -0,0 +1,179 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the MIT License (MIT).
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Windows.Devices.I2c;
+using Windows.Devices.Enumeration;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Devices.Spi;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SDKTemplate
+{
+    public sealed partial class Scenario1_ReadData : Page
+    {
+        private SpiDevice _mcp3008;
+        private I2cDevice sensor;
+        private DispatcherTimer timer;
+        private bool flag = true;
+
+        public Scenario1_ReadData()
+        {
+            this.InitializeComponent();
+            //_deviceClient = DeviceClient.Create(IotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey(DeviceId, DeviceKey), TransportType.Mqtt);
+            //
+            //SendDeviceToCloudMessagesAsync();
+            //
+            ////ReceiveC2dAsync();
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            StopScenario();
+        }
+
+        private async Task StartScenarioAsync()
+        {
+            string i2cDeviceSelector = I2cDevice.GetDeviceSelector();
+            IReadOnlyList<DeviceInformation> devices = await DeviceInformation.FindAllAsync(i2cDeviceSelector);
+
+            var sensor_settings = new I2cConnectionSettings(0x48);
+
+            // This will result in a NullReferenceException in Timer_Tick below.
+            sensor = await I2cDevice.FromIdAsync(devices[0].Id, sensor_settings);
+
+
+            //using SPI0 on the Pi
+            var spiSettings = new SpiConnectionSettings(0);//for spi bus index 0
+            spiSettings.ClockFrequency = 3600000; //3.6 MHz
+            spiSettings.Mode = SpiMode.Mode0;
+            
+            string spiQuery = SpiDevice.GetDeviceSelector("SPI0");
+            //using Windows.Devices.Enumeration;
+            var deviceInfo = await DeviceInformation.FindAllAsync(spiQuery);
+            if (deviceInfo != null && deviceInfo.Count > 0 && flag)
+            {
+            
+                _mcp3008 = await SpiDevice.FromIdAsync(deviceInfo[0].Id, spiSettings);
+                textSPI.Text = "SPI is connected!!! :-)";
+                flag = false;
+            }
+            else if (flag)
+            {
+                textSPI.Text = "SPI Device Not Found :-(";
+            }
+
+             // Start the polling timer.
+             timer = new DispatcherTimer() { Interval = TimeSpan.FromMilliseconds(500) };
+             timer.Tick += Timer_Tick;
+             timer.Tick += humidityRead;
+             timer.Start();
+             
+        }
+
+        void StopScenario()
+        {
+            if (timer != null)
+            {
+                timer.Tick -= Timer_Tick;
+                timer.Tick -= humidityRead;
+                timer.Stop();
+                timer = null;
+            }
+
+            // Release the I2C sensor.
+            if (sensor != null)
+            {
+                sensor.Dispose();
+                sensor = null;
+            }
+            
+        }
+
+        async void StartStopScenario()
+        {
+            Debug.WriteLine("Device Activated\n");
+            await AzureIoTHub.SendDeviceToCloudMessageAsync();
+            Debug.WriteLine("after msg\n");
+
+            if (timer != null)
+            {
+                StopScenario();
+                StartStopButton.Content = "Start";
+                ScenarioControls.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
+            }
+            else
+            {
+                StartStopButton.IsEnabled = false;
+                await StartScenarioAsync();
+                StartStopButton.IsEnabled = true;
+                StartStopButton.Content = "Stop";
+                ScenarioControls.Visibility = Windows.UI.Xaml.Visibility.Visible;
+            }
+
+        }
+
+        private void Timer_Tick(object sender, object e)
+        {
+            // Read data from I2C.
+            var command = new byte[1];
+            var temperatureData = new byte[2];
+
+
+            // If this next line crashes with a NullReferenceException, then
+            // there was a sharing violation on the device. (See StartScenarioAsync above.)
+            //
+            // If this next line crashes for some other reason, then there was
+            // an error accessing the device.
+
+            // Read temperature.
+            command[0] = 0x48;
+            // If this next line crashes, then there was an error accessing the sensor.
+            sensor.WriteRead(command, temperatureData);
+
+            // Calculate and report the temperature.
+            var rawTempReading = temperatureData[0] << 8 | temperatureData[1];
+            var rawShifted = rawTempReading >> 4;
+            float temperature = rawShifted * 0.0625f;
+            CurrentTemp.Text = temperature.ToString();
+        }
+
+        private void humidityRead(object sender, object e)
+        {
+            //From data sheet -- 1 byte selector for channel 0 on the ADC
+            //First Byte sends the Start bit for SPI
+            //Second Byte is the Configuration Bit
+            //1 - single ended 
+            //0 - d2
+            //0 - d1
+            //0 - d0
+            //             S321XXXX <-- single-ended channel selection configure bits
+            var transmitBuffer = new byte[3] { 1, 0x80, 0x00 };
+            var receiveBuffer = new byte[3];
+
+            _mcp3008.TransferFullDuplex(transmitBuffer, receiveBuffer);
+
+            var result = ((receiveBuffer[1] & 3) << 8) + receiveBuffer[2];
+            var output = result;
+            CurrentHumidity.Text = output.ToString();
+
+        }
+    }
+}
